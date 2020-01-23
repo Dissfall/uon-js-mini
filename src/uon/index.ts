2eff41c779439afa357fbd01ad6691a4a424c7c3
dissfall <dissfalling@gmail.com>
2020-01-23 13:57:21 +0300

    feat: init wrapper

---
+++ src/uon/index.ts
@@ -0,0 +1,149 @@
+import Queue = require('smart-request-balancer')
+import axios from 'axios'
+
+import {
+  UONReq,
+  UONCountriesRes,
+  UONCountryCreateRes,
+  UONManager,
+  UONManagerRes,
+  UONUserCreateReq,
+  UONUserRes,
+} from './types'
+
+export { UONReq }
+
+const queue = new Queue({
+  rules: {
+    common: {
+      rate: 5,
+      limit: 1,
+      priority: 1,
+    },
+  },
+  default: {
+    //@ts-ignore
+    rate: 5,
+    limit: 1,
+  },
+  overall: {
+    rate: 5,
+    limit: 1,
+  },
+  retryTime: 300,
+  ignoreOverallOverheat: true,
+})
+
+class UON {
+  private base: string = 'https://api.u-on.ru/'
+
+  constructor(private token: string) {}
+
+  private url(path: string): string {
+    return this.base + this.token + '/' + path
+  }
+
+  public async request<T>(path: string, payload?: UONReq): Promise<T> {
+    return new Promise(async (resolve, reject) => {
+      if (!payload) {
+        queue.request(
+          (retry: any) =>
+            axios
+              .get(this.url(path))
+              .then(res => {
+                resolve(res.data)
+              })
+              .catch(err => {
+                if (err.response?.status === 429) {
+                  console.log('To many reqests. Waitig...')
+                  return retry(err.response.data.parameters?.retry_after)
+                }
+              }),
+          Math.floor(Math.random() * 1000),
+          'common'
+        )
+      } else {
+        queue.request(
+          (retry: any) =>
+            axios
+              .post(this.url(path), payload)
+              .then(res => {
+                resolve(res.data)
+              })
+              .catch(err => {
+                if (err.response?.status === 429) {
+                  console.log('To many reqests. Waitig...')
+                  return retry(err.response.data.parameters?.retry_after)
+                }
+              }),
+          Math.floor(Math.random() * 1000),
+          'common'
+        )
+      }
+    })
+  }
+
+  public async accessTest(): Promise<void> {
+    console.log('Testig access')
+    try {
+      await this.countries
+      console.log('Succes')
+      return Promise.resolve()
+    } catch {
+      console.log('Failed')
+      return Promise.reject()
+    }
+  }
+
+  public get countries(): Promise<UONCountriesRes> {
+    const path = 'countries'
+    return this.request<UONCountriesRes>(path)
+  }
+
+  public async createCountry(
+    name: string,
+    nameEN?: string
+  ): Promise<UONCountryCreateRes> {
+    const path = 'country/create'
+    return await this.request<UONCountryCreateRes>(path, {
+      name: name,
+      name_en: nameEN,
+    })
+  }
+
+  public get managers(): Promise<UONManagerRes> {
+    const path = 'manager'
+    return this.request<UONManagerRes>(path)
+  }
+
+  public async createUser(data: UONUserCreateReq): Promise<any> {
+    const path = 'user/create'
+    return this.request<any>(path, data)
+  }
+
+  public async getManagerByNote(number: number | string): Promise<UONManager> {
+    const managers = await this.managers
+    const manager = managers.users.find(x => x.u_note === String(number))
+
+    if (manager) {
+      return Promise.resolve(manager)
+    } else {
+      return Promise.reject()
+    }
+  }
+
+  public async getTouristByPhone(phone: number | string): Promise<UONUserRes> {
+    const path = 'user/phone/' + phone
+    return this.request<UONUserRes>(path)
+  }
+
+  public async touristUpdate(
+    id: number | string,
+    data: UONUserCreateReq
+  ): Promise<any> {
+    const path = 'user/update/' + id
+    return this.request<any>(path, data)
+  }
+}
+
+export default UON
---
+++ src/uon/types.ts
@@ -0,0 +1,92 @@
+export type UONRes = {} & (UONCountriesRes)
+
+export type UONReq = (UONCountryCreateReq | UONManagerRes | UONUserCreateReq)
+
+export interface UONErrorRes {
+  result: number
+  message: string
+  id?: number
+}
+
+export interface UONCountriesRes {
+  records: {
+    [index: number]: {
+      id: number
+      name: string
+      name_en: string
+    }
+  }
+}
+
+export type UONManagerRes = { users: UONManager[] };
+
+export interface UONManager {
+  global_u_id: number
+  u_id: number
+  u_type: number
+  tourist_kind: number
+  u_surname: string
+  u_name: string
+  u_sname: string
+  u_surname_en: string
+  u_name_en: string
+  u_email: string
+  u_sex: number
+  u_phone: string
+  u_phone_mobile: string
+  u_phone_home: string
+  u_passport_number: string
+  u_passport_taken: string
+  u_zagrau_number: string
+  u_birthday: string
+  manager_id: number
+  u_note: string
+}
+
+export interface UONCountryCreateReq {
+  name: string
+  name_en?: string
+}
+
+export type UONCountryCreateRes = any
+
+export interface UONUserCreateReq {
+  u_type?: number
+  u_name?: string
+  u_surname?: string
+  u_sname?: string
+  u_name_en?: string
+  u_surname_en?: string
+  u_address?: string
+  u_phone?: string
+  u_email?: string
+  u_birthday?: string
+  u_passport_number?: string
+  u_passport_taken?: string
+  u_zagran_number?: string
+  u_zagran_expire?: string
+  u_zagran_organization?: string
+  u_note?: string
+}
+
+export interface UONUserRes {
+  users: [{
+    u_id: number
+    u_type: number
+    u_name: string
+    u_surname: string
+    u_sname: string
+    u_name_en?: string
+    u_surname_en?: string
+    u_address?: string
+    u_phone?: string
+    u_email?: string
+    u_birthday?: string
+    u_passport_number?: string
+    u_passport_taken?: string
+    u_zagran_number?: string
+    u_zagran_expire?: string
+    u_zagran_organization?: string
+    u_note?: string
+  }]
+}
